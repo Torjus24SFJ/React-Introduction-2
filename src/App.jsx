f4f2dbc184f918d679213ab4e2ebdb832b8c29a6
Torjus <Torjus24sfj@gmail.com>
2025-01-14 12:29:37 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <header className={style.header}>
+        <h1>React component Variations</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
